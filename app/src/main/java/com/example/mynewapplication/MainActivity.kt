1130d03a474faa06e9fc073c7ef593c0417bda15
seeta11 <seeta141722@gmail.com>
2022-01-09 02:24:46 +0300

    compete Activity

---
+++ app/src/main/java/com/example/mynewapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mynewapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    //val apiKey = "8d6d12d5cd91d80cc1c8c45439cf01cf"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val button = findViewById<Button>(R.id.searchButton)
+       // button.setOnClickListener {
+      //      getWeather()
+       // }
+   // }
+   // private fun getWeather() {
+       // val searchInput = findViewById<EditText>(R.id.searchInput)
+       // try {
+           // if (searchInput.text.isNotEmpty()) {
+            //    var fullURL = "http://api.openweathermap.org/data/2.5/forecast?id=&524901&units=metric&appid=$apiKey"
+
+          //  }
+       // }catch (err:Exception){
+
+      //  }
+    }
+}
